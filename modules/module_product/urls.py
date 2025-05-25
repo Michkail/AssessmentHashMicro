e8672ee9dd6cf3a2f568c7e8ba6179eb2c499a20
Michkail <pitermichkail@ymail.com>
2025-05-26 01:37:26 +0700

    fix: fixing bug

--- modules/module_product/urls.py
+++ modules/module_product/urls.py
@@ -5,6 +5,6 @@ from .views import (ProductListView, ProductCreateView,
 urlpatterns = [
     path('', ProductListView.as_view(), name='product-list'),
     path('create/', ProductCreateView.as_view(), name='product-create'),
-    path('<int:pk>/edit/', ProductUpdateView.as_view(), name='product-edit'),
-    path('<int:pk>/delete/', ProductDeleteView.as_view(), name='product-delete')
+    path('update/<int:pk>/', ProductUpdateView.as_view(), name='product-update'),
+    path('delete/<int:pk>/', ProductDeleteView.as_view(), name='product-delete')
 ]
